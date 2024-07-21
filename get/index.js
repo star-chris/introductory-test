d827d82c4df55725b9bee5cf832bf8dbd4d77878
star-chris <112868341+star-chris@users.noreply.github.com>
2024-07-21 05:43:37 +0100

    Update index.js

--- get/index.js
+++ get/index.js
@@ -1,30 +1,14 @@
-//let buttonOnPage = document.getElementById("btn");
+let buttonOnPage = document.getElementById("btn");
 
-//buttonOnPage.addEventListener("click", greetUser);
+buttonOnPage.addEventListener("click", greetUser);
 
-//let colors = [ 'green', 'red', 'yellow', 'purple', 'pink', 'brown', 'white', 'peach', 'gey','gold', 'blue']; // Array of colors
-//let currentColorIndex = 0;
+let colors = ['green', 'red', 'yellow', 'purple', 'pink', 'brown', 'white', 'peach']; // Array of colors
+let currentColorIndex = 0;
 
-//function greetUser() {
-  //console.log('welcome guests');
-  //document.body.style.backgroundColor = colors[currentColorIndex];
-  //currentColorIndex = (currentColorIndex + 1) % colors.length; // Cycle through the colors
-//}
-
-
-
-
-
-  let buttonOnPage = document.getElementById("btn");
-
-  buttonOnPage.addEventListener("click", greetUser);
-
-  let colors = [ 'green', 'red', 'yellow', 'purple', 'pink', 'brown', 'white', 'peach']; // Array of colors
-  let currentColorIndex = 0;
-
-  function greetUser() {
-    console.log('welcome guests');
+function greetUser() {
+    console.log('Welcome guests');
     document.body.style.backgroundColor = colors[currentColorIndex];
     currentColorIndex = (currentColorIndex + 1) % colors.length; // Cycle through the colors
-  }
+}
+
 
